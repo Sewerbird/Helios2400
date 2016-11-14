502f1ac4b5fc19f66082b3bdc78778ac1898541c
Edward Miller <sewerbird@gmail.com>
2016-11-14 23:24:27 +0800

    Skeletons of gamestate manipulation

---
+++ src/state/MutatorBus.lua
@@ -0,0 +1,22 @@
+--MutatorBus.lua
+local class = require 'lib/30log'
+local MutatorBus = class('MutatorBus', {
+
+})
+
+function MutatorBus:queue ( )
+end
+
+function MutatorBus:stepForward ( )
+end
+
+function MutatorBus:stepBackward ( )
+end
+
+function MutatorBus:rewind ( )
+end
+
+function MutatorBus:playAll ( )
+end
+
+return MutatorBus
