107db2b7f87ececb3b7befa8bcab8b74b3121583
Edward Miller <sewerbird@gmail.com>
2016-11-20 17:34:58 +0800

    Mutator bus implemented, untested

--- src/state/MutatorBus.lua
+++ src/state/MutatorBus.lua
@@ -1,22 +1,44 @@
 --MutatorBus.lua
 local class = require 'lib/30log'
-local MutatorBus = class('MutatorBus', {
+local Queue = require 'src/structure/Queue'
+local Stack = require 'src/structure/Stack'
 
+local MutatorBus = class('MutatorBus', {
+	history = Stack:new(),
+	rewound = Stack:new(),
+	bus = Queue:new()
 })
 
-function MutatorBus:queue ( )
+function MutatorBus:queue ( ... )
+	self.bus:push(arg)
+end
+
+function MutatorBus:queueAll( arrMut )
+	self:queue(unpack(arrMut))
 end
 
 function MutatorBus:stepForward ( )
+	local mutation = self.rewound:pop()
+	mutation:apply()
+	self.history:push(mutation)
 end
 
-function MutatorBus:stepBackward ( )
+function MutatorBus:stepBack ( )
+	local mutation = self.history:pop()
+	mutation:rollback()
+	self.rewound:push(mutation)
 end
 
-function MutatorBus:rewind ( )
+function MutatorBus:playNext ()
+	local mutation = self.bus:pop()
+	mutation:apply()
+	self.history:push(mutation)
 end
 
-function MutatorBus:playAll ( )
+function MutatorBus:playAll ()
+	while self.bus:getn() > 0 do
+		self:playNext()
+	end
 end
 
 return MutatorBus
