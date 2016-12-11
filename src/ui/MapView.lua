7a28d85b30eede6f21400f806e209d976564e23a
Edward Miller <sewerbird@gmail.com>
2016-12-11 16:47:49 +0800

    fix parenting

--- src/ui/MapView.lua
+++ src/ui/MapView.lua
@@ -73,12 +73,10 @@ function MapView:init( registry, scenegraph, tiles, cities, units )
 
   scenegraph:attach(Map_View)
   scenegraph:attachAll({Map_Layer,UI_Layer}, Map_View)
-  scenegraph:attachAll({Tile_Layer,City_Layer,Unit_Layer,UI_Layer}, Map_Layer)
-  scenegraph:attachAll(tiles, Tile_Layer)
-  scenegraph:attachAll(cities, City_Layer)
-  scenegraph:attachAll(units, Unit_Layer)
+  scenegraph:attachAll({Tile_Layer,City_Layer,Unit_Layer}, Map_Layer)
   scenegraph:attachAll({Inspector},UI_Layer)
   scenegraph:attachAll({Quick_Command_Panel_View.root},Inspector)
+
   for i, tile in ipairs(tiles) do
     scenegraph:attach(tile.root, Tile_Layer)
   end
