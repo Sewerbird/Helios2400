915f9a3bba3e65f5dbdb0f8f5acc6778a34582f4
thimo visser <thimo.visser@hotmail.nl>
2016-12-15 16:04:55 +0800

    split pathing mutator into multiple smaller mutators

--- src/system/SelectableSystem.lua
+++ src/system/SelectableSystem.lua
@@ -77,8 +77,13 @@ function SelectableSystem:init (registry, targetCollection, cursor_sprite)
 				local budget = self.registry:get(self.registry:get(self.current_selection):getComponent("Stateful").ref):getComponent("GameInfo").curr_move
 				self:displayPathOverlay(msg.map, budget)
 			end,
-			onreclickedOtherHex = function(this, event, from, to, msg) 
-				if self:moveSelectedTo(msg.uid, msg.address) then
+			onreclickedOtherHex = function(this, event, from, to, msg)
+				local success = true
+				for i,v in ripairs(self.path) do
+					local cost = self.path_costs[i] - (self.path_costs[i + 1] or 0)
+					if success and not self:moveSelectedTo(msg.uid, v, cost) then success = false end
+				end
+				if success then
 					self.fsm:movingDoneReady(msg)
 				else
 					self.fsm:movingDoneAborted(msg)
@@ -225,7 +230,7 @@ function SelectableSystem:pathTo(fromAddress, toAddress, map)
 	print("Path: " .. inspect(self.path) .. "\nTotal Cost: " .. inspect(self.path_cost) .. "\nPiecewise Costs: " .. inspect(self.path_costs))
 end
 
-function SelectableSystem:moveSelectedTo (tgtGameObjectId, tgtAddress)
+function SelectableSystem:moveSelectedTo (tgtGameObjectId, tgtAddress, cost)
 	if self.current_selection ~= nil and self.current_selection ~= tgtGameObjectId then --something might be moveable
 		local srcObj = self.registry:get(self.current_selection)
 		local dstObj = self.registry:get(tgtGameObjectId)
@@ -237,7 +242,7 @@ function SelectableSystem:moveSelectedTo (tgtGameObjectId, tgtAddress)
 				dstObj:getComponent('Stateful').ref,
 				srcObj:getComponent('Placeable').address, 
 				dstObj:getComponent('Addressable').address, 
-				self.path_cost)
+				cost)
 
 			if mutMove:isValid(self.registry) then
 				self.registry:publish("IMMEDIATE_MUTATE", mutMove)
