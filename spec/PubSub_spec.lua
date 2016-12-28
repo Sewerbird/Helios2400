364d6fa50a901319db0123fad994bd66bb723687
Edward Miller <sewerbird@gmail.com>
2016-12-28 21:28:15 +0800

    remove tests that only test constructor rudimentarily

--- spec/PubSub_spec.lua
+++
@@ -1,9 +0,0 @@
-inspect = require 'lib/inspect'
-PubSub = require 'src/PubSub' 
-
-describe('PubSub', function ()
-	it('should initialize correctly', function ()
-		local my_PubSub = PubSub:new()
-		assert.truthy(my_PubSub)
-	end)
-end)
--- spec/TouchDelegate_spec.lua
+++
@@ -1,9 +0,0 @@
-inspect = require 'lib/inspect'
-TouchDelegate = require 'src/datatype/TouchDelegate' 
-
-describe('TouchDelegate', function ()
-	it('should initialize correctly', function ()
-		local my_TouchDelegate = TouchDelegate:new()
-		assert.truthy(my_TouchDelegate)
-	end)
-end)
