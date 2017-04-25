d2bbc29f785f6afc7c414167617a766477186f90
thimo visser <thimo.visser@hotmail.nl>
2017-04-25 18:05:02 +0200

    added simple scifi theme for titlescreen

--- src/gamestate/TitleScreen.lua
+++ src/gamestate/TitleScreen.lua
@@ -5,14 +5,17 @@ local Text = require 'lib/LoveGUI/core/text'
 local TitleScreen = Container.new('FULL_WINDOW',{visible = false, ori = 'hor', alignment = 'center'})
 local JoinDialog = require 'src/gamestate/JoinDialog'
 
+local SciFiTheme = require('src/theme/scifi')
+TitleScreen:setTheme(SciFiTheme)
+
 TitleScreen:addElement(
-    Container.new('MENU_CENTERING_CONTAINER',{width = '35%',alignment = 'center', visible = false, orientation = 'ver'}):addElement(
-        Container.new('MENU_OPTIONS',{height = '70%',alignment = 'top', orientation = 'ver'})
+    Container.new('MENU_CENTERING_CONTAINER',{width = '45%',alignment = 'center', visible = false, orientation = 'ver'}):addElement(
+        Container.new('MENU_OPTIONS',{height = '90%',alignment = 'top', orientation = 'ver'})
     )
 )
 
 TitleScreen:getElement('MENU_OPTIONS'):addElements({
-    Text.new('TITLE_TEXT',{textLoc = 'center',text = 'Helios 2400', height = 50}),
+    Text.new('TITLE_TEXT',{textLoc = 'center',text = 'Helios 2400', height = 50, font = love.graphics.newFont(24)}),
     Button.new('JOIN_GAME',{margins = 20, height = '8%', text = 'Join a Game', onRelease = function(self)
         Global.Viewer.Systems.UIStack:push(JoinDialog)
     end}),
---
+++ src/theme/scifi.lua
@@ -0,0 +1,35 @@
+local Theme = require('lib/LoveGUI/core/theme')
+local theme = Theme.new()
+
+local LIGHT_BLUE = {51,204,255}
+local BLUE = {34,136,187}
+local DARK_BLUE = {17,68,119}
+local DARKER_BLUE = {0,0,51}
+
+function drawBasicRectangle(x,y,w,h)
+end
+
+theme:set('container','onDraw',function(self)
+	local x,y,w,h = self:getRectangle()
+
+	love.graphics.setColor(BLUE)
+	love.graphics.rectangle('fill', x, y, w, h)
+	love.graphics.setColor(DARKER_BLUE)
+	love.graphics.rectangle('line', x, y, w, h)
+end)
+
+theme:set('text','onDraw',function(self)
+	local x,y,w,h = self:getRectangle()
+
+	love.graphics.setColor(DARK_BLUE)
+	love.graphics.rectangle('fill', x, y, w, h)
+	love.graphics.setColor(DARKER_BLUE)
+	love.graphics.rectangle('line', x, y, w, h)
+	
+	love.graphics.setColor(LIGHT_BLUE)
+	local tx, ty = self:getTextLocation()
+	love.graphics.setFont(self:getFont())
+	love.graphics.print(self:getText(), tx,ty)
+end)
+
+return theme
